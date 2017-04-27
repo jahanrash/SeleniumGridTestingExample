d6a9513ef9d4995796a25e1c878d9132d5acc283
dehnel <adam.dehnel@biworldwide.com>
2017-04-27 15:16:38 -0500

    Commented out correct test class

--- src/test/java/bi/com/seleniumgrid/PhantomJsTest.java
+++ src/test/java/bi/com/seleniumgrid/PhantomJsTest.java
@@ -1,79 +1,79 @@
-// /*
-//  * (C) Copyright 2016 Boni Garcia (http://bonigarcia.github.io/)
-//  *
-//  * Licensed under the Apache License, Version 2.0 (the "License");
-//  * you may not use this file except in compliance with the License.
-//  * You may obtain a copy of the License at
-//  *
-//  *   http://www.apache.org/licenses/LICENSE-2.0
-//  *
-//  * Unless required by applicable law or agreed to in writing, software
-//  * distributed under the License is distributed on an "AS IS" BASIS,
-//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  * See the License for the specific language governing permissions and
-//  * limitations under the License.
-//  *
-//  */
+/*
+ * (C) Copyright 2016 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
-// package bi.com.seleniumgrid;
+package bi.com.seleniumgrid;
 
-// import org.junit.After;
-// import org.junit.Before;
-// import org.junit.BeforeClass;
-// import org.junit.Test;
-// import org.openqa.selenium.By;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.phantomjs.PhantomJSDriver;
-// import org.openqa.selenium.support.ui.ExpectedConditions;
-// import org.openqa.selenium.support.ui.WebDriverWait;
-// import org.testng.Assert;
-// import io.github.bonigarcia.wdm.PhantomJsDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import io.github.bonigarcia.wdm.PhantomJsDriverManager;
 
-// /**
-//  * Test with PhantomJS.
-//  *
-//  * @author Boni Garcia (boni.gg@gmail.com)
-//  * @since 1.0.0
-//  */
-// public class PhantomJsTest {
+/**
+ * Test with PhantomJS.
+ *
+ * @author Boni Garcia (boni.gg@gmail.com)
+ * @since 1.0.0
+ */
+public class PhantomJsTest {
 
-//   private WebDriver driver;
+  private WebDriver driver;
 
-//   @BeforeClass
-//   public static void setupClass() {
-//     PhantomJsDriverManager.getInstance().setup();
-//   }
+  @BeforeClass
+  public static void setupClass() {
+    PhantomJsDriverManager.getInstance().setup();
+  }
 
-//   @Before
-//   public void setupTest() {
-//     driver = new PhantomJSDriver();
-//   }
+  @Before
+  public void setupTest() {
+    driver = new PhantomJSDriver();
+  }
 
-//   @After
-//   public void teardown() {
-//     if (driver != null) {
-//       driver.quit();
-//     }
-//   }
+  @After
+  public void teardown() {
+    if (driver != null) {
+      driver.quit();
+    }
+  }
 
-//   @Test
-//   public void test() {
-//     // Your test code here. For example:
-//     WebDriverWait wait = new WebDriverWait(driver, 30); // 30 seconds of timeout
-//     driver.get("https://en.wikipedia.org/wiki/Main_Page"); // navigate to Wikipedia
+  @Test
+  public void test() {
+    // Your test code here. For example:
+    WebDriverWait wait = new WebDriverWait(driver, 30); // 30 seconds of timeout
+    driver.get("https://en.wikipedia.org/wiki/Main_Page"); // navigate to Wikipedia
 
-//     pageTitgle = driver.getTitle().trim();
-//     Assert.assertEquals(pageTitgle, "GoalQuest");    
+    pageTitgle = driver.getTitle().trim();
+    Assert.assertEquals(pageTitgle, "GoalQuest");    
 
-//     By searchInput = By.id("searchInput"); // search for "Software"
-//     wait.until(ExpectedConditions.presenceOfElementLocated(searchInput));
-//     driver.findElement(searchInput).sendKeys("Software");
-//     By searchButton = By.id("searchButton");
-//     wait.until(ExpectedConditions.elementToBeClickable(searchButton));
-//     driver.findElement(searchButton).click();
+    By searchInput = By.id("searchInput"); // search for "Software"
+    wait.until(ExpectedConditions.presenceOfElementLocated(searchInput));
+    driver.findElement(searchInput).sendKeys("Software");
+    By searchButton = By.id("searchButton");
+    wait.until(ExpectedConditions.elementToBeClickable(searchButton));
+    driver.findElement(searchButton).click();
 
-//     wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"),
-//         "Computer software")); // assert that the resulting page contains a text
-//   }
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"),
+        "Computer software")); // assert that the resulting page contains a text
+  }
 
-// }
+}
